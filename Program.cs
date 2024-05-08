7eb962c8ef978378509b4750801c0cacdaab1e79
CheeseOmelette <poketvgameplays@gmail.com>
2024-05-08 15:30:53 -0300

    Mensagen!

--- Program.cs
+++ Program.cs
@@ -1,3 +1,19 @@
 ﻿using DesafioPOO.Models;
 
+
+
+Console.WriteLine("Este é o Iphone");
+Smartphone iphone = new Iphone(numero: "999999", modelo: "15 Pro", imei: "13914120", memoria: 64);
+iphone.Ligar();
+iphone.InstalarAplicativo("Netflix");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Este é o Nokia");
+Smartphone nokia = new Nokia(numero: "666666", modelo: "Modelo 1", imei: "2141512", memoria: 32);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+
+
 // TODO: Realizar os testes com as classes Nokia e Iphone
